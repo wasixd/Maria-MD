21d31003d92244432f3135f791e2e55234bac92a
the  develpor <164501586+wasixd@users.noreply.github.com>
2024-03-30 16:46:29 +0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,27 +2,27 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = ['923135673658']
+global.ownername = "💙𓆩‎✘WASI_✰"//owner name
+global.ytname = "YT: wasitech10"
+global.socialm = "GitHub: Itxxwasi"
+global.location = "Pakistan"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'kashmir MD' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = '💙𓆩WASI𓆪_✰'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'waso'
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
+global.link = 'https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j'
 
 //custom prefix
-global.prefa = ['.']
+global.prefa = ['/']
 
 //false=disable and true=enable
 global.welcome = false //auto welcome
@@ -38,7 +38,7 @@ global.autoread_status = false //auto view status/story
 
 //reply messages
 global.mess = {
-    done: '*here you go!* \n\n*🎀 Maria Bot 🎀*\n\n*🧩 Bot link:* \nhttps://github.com/AYUSH-PANDEY023/Maria-MD\n',
+    done: '*here you go!* \n\n*🎀 KASHMIR MD BY WASI 🎀*\n\n*🧩 Bot link:* \nhttps://github.com/wasixd/WASI-MD \n',
     prem: '*This feature can be used by premium user only*',
     admin: '*This feature can be used by admin only*',
     botAdmin: '*This feature can only be used when the bot is a group admin* ',
